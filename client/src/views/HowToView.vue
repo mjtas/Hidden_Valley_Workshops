8c236135a5f8affe8c716b85d2447124c0750582
mjtas <101754279+mjtas@users.noreply.github.com>
2024-07-07 22:50:26 +1000

    Fix references to selectedGuideTitle in HowToView

--- client/src/views/HowToView.vue
+++ client/src/views/HowToView.vue
@@ -7,12 +7,12 @@
           </div>
           <div class="main-other">
           <div class="guide-left">
-            <HowToList :guides="guides" @guideSelected="handleGuideSelected" />
+            <HowToList :guides="guides" @guideSelected="handleGuideSelected(selectedGuideTitle)" />
           </div>
           <div class="guide-right">
             <ul class="main-text">
-              <li class="main-li" v-if="selectedGuideName==''">{{ howToIntro1 }}</li>
-              <li class="main-li" v-if="selectedGuideName==''">{{ howToIntro2 }}</li>
+              <li class="main-li" v-if="selectedGuideTitle==''">{{ howToIntro1 }}</li>
+              <li class="main-li" v-if="selectedGuideTitle=''">{{ howToIntro2 }}</li>
 
               <SelectedGuideComponent guide="selectedGuide" v-else />
             </ul>
@@ -36,7 +36,7 @@
       return {
         guides: [], // Store fetched data here
         selectedGuide: null, //Set initial value to null
-        selectedGuideName: '', // Set initial value to an empty string
+        selectedGuideTitle: '', // Set initial value to an empty string
         howToIntro1: "I love a good ‘how to’ guide. But let’s face it, a good one can be hard to find! Either they use expensive materials, they use inches (a hassle to convert), or they leave out important details and leave you without any idea of ‘how to’! I have been getting things done for myself around the house for a long time now, and I’m finally getting to the point where I feel like I know enough about what I’m doing to share my own ‘how to’ guides. I have done all of these projects for myself and found them really useful, but I have stuck to how I completed each one - which is not to say you could find another way or even a better way! I’d love to improve these guides and add more in over time so if you have any suggestions please email me.", 
         howToIntro2: "Each project is rated with a difficulty rating. Easy are quick and easy to accomplish with minimal experience and/or tools. Medium projects require some more experience and time to get it done. For example, medium rated building projects require the basic tool kit I recommend in my building workshops, and I’ll assume you also know how to use your tools to measure and cut accurately. Tricky projects are either more time consuming (taking multiple weekends to do) or require more specialised tools/ equipment."
       };
