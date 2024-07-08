2c9179a79237f8342b06f5bf93ec0a561ffb40c7
mjtas <101754279+mjtas@users.noreply.github.com>
2024-07-08 10:44:25 +1000

    Correct typo in SelectedGuideComponent

--- client/src/components/SelectedGuideComponent.vue
+++ client/src/components/SelectedGuideComponent.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <h3>{{ guide.title }}</h3>
-    <h3>Difficulty Level: {{ guide.rating }}</h3>
+    <h4>Difficulty Level: {{ guide.rating }}</h4>
     <img class="about-img" :src="`/${ guide.image }.png`"/>
-    <ul v-if="isBuilding">
+    <ul v-if="isBuilding()">
       <AssumedKnowledge />
     </ul>
     <ul>
-      <li>{{ guide.introText }}</li>
+      <li class="main-li">{{ guide.introText }}</li>
     </ul>
     <ToolsMaterials :guide="guide"/>
-    <table v-if="isBuilding">
+    <table v-if="isBuilding()">
       <CuttingList :guide="guide"/>
     </table>
     <NumberedSteps :guide="guide"/>
     <ul>
-      <li>{{ guide.concText }}</li>
+      <li class="main-li">{{ guide.concText }}</li>
     </ul>
   </div>
 </template>
