cb806ead1919a8a2795630874edd6e547601a9dc
mjtas <101754279+mjtas@users.noreply.github.com>
2024-08-19 21:27:40 +1000

    Filter out workshops with past dates

--- client/src/components/WorkshopTimetable.vue
+++ client/src/components/WorkshopTimetable.vue
@@ -10,7 +10,7 @@
       </thead>
       <tbody>
         <!-- Loop through each workshop in sortedWorkshops -->
-        <tr v-for="(workshop, index) in workshops" :key="index">
+        <tr v-for="(workshop, index) in filteredWorkshops" :key="index">
           <td>{{ formatDate(workshop.date) }}</td> <!-- Display formatted workshop date -->
           <td>
             <a @click="selectWorkshop(workshop.name)" class="workshop-a">{{ workshop.name }}</a>
@@ -26,15 +26,22 @@ export default {
     props: {
         workshops: Array, // Define the workshops prop to receive the sorted workshops data
     },
+    computed: {
+      // Filter workshops to exclude those before today's date
+      filteredWorkshops() {
+        const today = new Date().setHours(0, 0, 0, 0); // Get today's date without time
+        return this.workshops.filter(workshop => new Date(workshop.date) >= today);
+      }
+    },
     methods: {
-        selectWorkshop(workshopName) {
+      selectWorkshop(workshopName) {
         // Emit an event or update a data property with workshopName
         this.$emit('workshopSelected', workshopName);
-        },
-        formatDate(date) {
+       },
+      formatDate(date) {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(date).toLocaleDateString('en-UK', options);
-        },
+      },
     },
   }
 </script>
