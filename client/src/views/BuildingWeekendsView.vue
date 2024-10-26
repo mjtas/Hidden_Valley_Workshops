2a53975919b38ac828f2b7d7b2bbdf38a4834f71
mjtas <101754279+mjtas@users.noreply.github.com>
2024-10-26 20:53:40 +1100

    Add new building weekend content

--- client/src/views/BuildingWeekendsView.vue
+++ client/src/views/BuildingWeekendsView.vue
@@ -3,16 +3,15 @@
       <div class="main-container">
         <div class="other-heading">
           <h1>Hidden Valley</h1>
-          <h2>Garden Design and Practical Skills Consulting</h2>
+          <h2>Building Weekends for Women</h2>
           </div>
           <div class="about-other">
             <div class="block">
               <div class="main-left">
                 <ul class="main-text">
-                  <h3>Garden Design</h3>
-                  <li class="main-li">I have designed many edible gardens including one using permaculture principles, another one with high raised beds, and here the Hidden Garden is modelled on a traditional french potager and the Courtyard Garden is a pure Charles Dowding No Dig design. I also love flowers and have included many perennials, annuals, climbers and of course roses in my gardens. </li>
-                  <li class="main-li">Having set up my own 3/4 acre block in Hobart for self-sufficient food production (including dairy, meat, eggs, fruit and vegetables) I can help you design your own plan for becoming more self-sufficient. I have learnt many lessons the hard way and look forward to saving others the money and heartache involved by helping you plan efficient and sensible systems to suit your block and lifestyle.</li>
-                  <li class="main-li">I believe our gardens can be expressions of who we are, they can be works of art, or they can be all about efficient food production. I’ll design the garden you want and one that will suit your needs.</li>
+                  <li class="main-li">Do you want more opportunities to practice your building skills?. Are you a complete beginner and want to learn the basics of building in a supportive all-female environment?</li>
+                  <li class="main-li">Hidden Valley is a one hour drive from Launceston or Devonport.  If you don't live in the area, then what better reason to have a weekend away than to learn building skills and gain confidence with tools at Hidden Valley. We’re only 10 minutes from the Golden Valley caravan park and 20 minutes from Deloraine where there are many options for dinner and self-contained rooms. </li>
+                  <li class="main-li">These weekends are designed specifically for beginners. I know it’s daunting to pick up tools if you’ve never done it before. But in a supportive, all-women environment and with my expert guidance, you’ll discover just how much you’re capable of.</li>
                 </ul>
               </div>
               <div class="main-right">
@@ -24,8 +23,25 @@
             <div class="block">
               <div class="main-left">
                 <ul class="main-text">
-                  <h3>Practical Skills Consulting</h3>
-                  <li class="main-li">You might want some help pruning your fruit trees. You might want some building advice on how to build a garden fence, greenhouse, chook house or any other DIY project you have planned. I’m here to help get you organised and get you started, whether that means telling you what tools you need, showing you the skills you need to get the job done, or working with you until you are confident enough to do it alone.</li>
+                  <li class="main-li">You will get:</li>
+                  <li class="main-li">Hands-On Learning: build your knowledge of and confidence with hand and power tools</li>
+                  <li class="main-li">Practical Skills: get confident with the techniques needed to confidently tackle DIY projects on your own</li>
+                  <li class="main-li">Supportive Environment</li>
+                  <li class="main-li">Personal Empowerment: you won’t be just learning building skills, you’ll also be building your confidence, overall practical skills, and self-reliance.</li>
+                </ul>
+                </div>
+            <div class="main-right">
+              <div class="main-opaque">
+                <img class="about-img" src="../assets/skillsConsult.png" alt="Hidden Valley">
+              </div>
+            </div>
+          </div>
+          <div class="block">
+              <div class="main-left">
+                <ul class="main-text">
+                  <li class="main-li">You will get:n the Building Weekends for Women we will work together as a team on specific building projects, going through every step of each one while I support you to learn how to build. You can work at your own pace, and the small group of 6 means everyone will get the help they need while also getting to know each other over the provided morning tea and lunch. The small group also means workshops could book up quickly so if you’re keen book your spot as these topics won’t be repeated! Each day starts at 9 and we will knock off at 3:30.</li>
+                  <li class="main-li">Hands-On Learning: build your knowledge of and confidence with hand and power toolsI am an experienced Carpenter/ Joiner and while working as an apprentice I helped build new builds, renovations and I worked with an artisan joiner making cabinetry with plywood cupboards and native timbers. I renovated my last house before sale, and worked full time on my new house build, as well as building all of the sheds, the greenhouse, and everything else at Hidden Valley ranging from play sets to garden fences!</li>
+                  <li class="main-li">Practical Skills: get confident with the techniques needed to confidently tackle DIY projects on your own</li>
                 </ul>
                 </div>
             <div class="main-right">
@@ -36,8 +52,8 @@
           </div>
           <div class="other-heading">
           <div class="home-booking-container">
-              <div class="home-booking"><h3>Discuss your needs and arrange an initial site visit ($99 for 90 minutes)</h3>
-              <router-link to="/contactUs" tag="button" class="button button3">Contact</router-link></div>
+              <div class="home-booking"><h3>Secure Your Spot at a Building Weekend</h3>
+              <router-link to="/workshops" tag="button" class="button button3">Book Now</router-link></div>
             </div>
           </div>
         </div>
