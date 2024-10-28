2d1ec8e26d7b91689df3cb6f315fd9cf5d457c5f
mjtas <101754279+mjtas@users.noreply.github.com>
2024-10-28 17:45:11 +1100

    Split building weekends paras

--- client/src/views/BuildingWeekendsView.vue
+++ client/src/views/BuildingWeekendsView.vue
@@ -10,8 +10,8 @@
               <div class="main-left">
                 <ul class="main-text">
                   <li class="main-li">Do you want more opportunities to practice your building skills? Are you a complete beginner and want to learn the basics of building in a supportive all-female environment?</li>
-                  <li class="main-li">Hidden Valley is a one hour drive from Launceston or Devonport.  If you don't live in the area, then what better reason to have a weekend away than to learn building skills and gain confidence with tools at Hidden Valley. We’re only 10 minutes from the Golden Valley caravan park and 20 minutes from Deloraine where there are many options for dinner and self-contained rooms. </li>
-                  <li class="main-li">These weekends are designed specifically for beginners. I know it’s daunting to pick up tools if you’ve never done it before. But in a supportive, all-women environment and with my expert guidance, you’ll discover just how much you’re capable of.</li>
+                  <li class="main-li">Hidden Valley is a one hour drive from Launceston or Devonport.  If you don't live in the area, then what better reason to have a weekend away than to learn building skills and gain confidence with tools at Hidden Valley! We’re only 10 minutes from the Golden Valley caravan park and 20 minutes from Deloraine where there are many options for dinner and self-contained rooms. </li>
+                  <li class="main-li">These weekends are designed specifically for beginners. I know it’s daunting to pick up tools if you’ve never done it before. But in a supportive, all-women environment and with my expert guidance, you’ll discover just how much you’re capable of!</li>
                 </ul>
               </div>
               <div class="main-right">
@@ -40,6 +40,30 @@
               <div class="main-left">
                 <ul class="main-text">
                   <li class="main-li">In the Building Weekends for Women we will work together as a team on specific building projects, going through every step of each one while I support you to learn how to build. You can work at your own pace, and the small group of 6 means everyone will get the help they need while also getting to know each other over the provided morning tea and lunch. The small group also means workshops could book up quickly so if you’re keen book your spot as these topics won’t be repeated! Each day starts at 9 and we will knock off at 3:30.</li>
+                  <li class="main-li">I am an experienced Carpenter/ Joiner and while working as an apprentice I helped build new builds, renovations and I worked with an artisan joiner making cabinetry with plywood cupboards and native timbers. I renovated my last house before sale, and worked full time on my new house build, as well as building all of the sheds, the greenhouse, and everything else at Hidden Valley ranging from play sets to garden fences!</li>
+                </ul>
+                </div>
+            <div class="main-right">
+              <div class="main-opaque">
+                <img class="about-img" src="../assets/buildingExp.png" alt="Hidden Valley">
+              </div>
+            </div>
+          </div>
+          <div class="block">
+              <div class="main-left">
+                <ul class="main-text">
+                  <li class="main-li">In the Building Weekends for Women we will work together as a team on specific building projects, going through every step of each one while I support you to learn how to build. You can work at your own pace, and the small group of 6 means everyone will get the help they need while also getting to know each other over the provided morning tea and lunch. The small group also means workshops could book up quickly so if you’re keen book your spot as these topics won’t be repeated! Each day starts at 9 and we will knock off at 3:30.</li>
+                </ul>
+                </div>
+            <div class="main-right">
+              <div class="main-opaque">
+                <img class="about-img" src="../assets/veraBuilding.png" alt="Hidden Valley">
+              </div>
+            </div>
+          </div>
+          <div class="block">
+              <div class="main-left">
+                <ul class="main-text">
                   <li class="main-li">I am an experienced Carpenter/ Joiner and while working as an apprentice I helped build new builds, renovations and I worked with an artisan joiner making cabinetry with plywood cupboards and native timbers. I renovated my last house before sale, and worked full time on my new house build, as well as building all of the sheds, the greenhouse, and everything else at Hidden Valley ranging from play sets to garden fences!</li>
                 </ul>
                 </div>
