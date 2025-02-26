bcd20a8c55d4529180a0e9c0af557bee319e6b44
mjtas <101754279+mjtas@users.noreply.github.com>
2025-02-27 10:24:40 +1100

    Hide weekends page

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -7,7 +7,6 @@
           <router-link to="/">Home</router-link> 
           <router-link to="/about">About</router-link>
           <router-link to="/workshops">Workshops</router-link>
-          <router-link to="/buildingWeekends">Building Weekends</router-link>
         </ul>
         <!-- Right side links -->
         <ul class="header-ul header-ul-right" :class="{ displayHeaderLinks: displayHeaderLinks }">
